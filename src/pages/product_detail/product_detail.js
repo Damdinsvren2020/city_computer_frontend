bf82a4d1e10b335d88dc16348a68a9f4b30cb383
avarga-byte <78478893+avarga-byte@users.noreply.github.com>
2022-07-25 12:47:39 +0800

    update

--- src/pages/product_detail/product_detail.js
+++ src/pages/product_detail/product_detail.js
@@ -58,11 +58,15 @@ const Product_detail = () => {
   }, [history, refresh]);
 
   const addToCart = async () => {
-    let formdata = new FormData()
-    formdata.append("userID", user._id)
-    formdata.append("productID", singleProduct._id)
-
-    const { data } = await axios.post("/customer/wishList", formdata)
+    const price = singleProduct.offer ? (singleProduct.price - (singleProduct.price * singleProduct.offer / 100)) : singleProduct.price
+    const { data } = await axios.post("/customer/wishList", {
+      userID: user._id,
+      cartItems: {
+        price: price,
+        quantity: 1,
+        product: singleProduct._id
+      }
+    })
     if (data.success) {
       Swal.fire({
         icon: "success"
@@ -78,8 +82,8 @@ const Product_detail = () => {
       <div className="max-w-[1200px] mx-auto mt-[30px]">
         <div className="w-full flex">
           <div className="w-[500px] h-[500px]">
+            {singleProduct._id}
             <Product_details productImages={singleProduct} />
-            {/* <Product_image_gallery /> */}
           </div>
           <div className="w-[850px] h-[500px] ">
             <Home_Product_detail addToCart={() => addToCart()} detail={singleProduct} />
